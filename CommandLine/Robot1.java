c653a5d5abf6bf6ce86e8aef7170e30597672ab1
ivan krat <ivan.krat@stydent.rjuhsd.us>
2018-10-04 09:09:25 -0700

    COPY MAIN ROBOT

--- CommandLine/Robot1.java
+++ CommandLine/Robot1.java
@@ -1,3 +1,18 @@
-public class Robot1 {
+import edu.cmu.ri.createlab.hummingbird.HummingbirdRobot;
 
+public class Robot1 extends HummingbirdRobot {
+    public static void main(String[] args) throws InterruptedException{
+        HummingbirdRobot bob = new HummingbirdRobot();
+
+
+        bob.setMotorVelocity(1,0);
+        int x = 1;
+        while(x<100000) {
+            int s = bob.getSensorValue(1);
+            System.out.println("Sensor: " + s);
+        }
+
+
+        bob.disconnect();
+    }
 }
