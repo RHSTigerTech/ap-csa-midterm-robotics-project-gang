d92f9e24eece37e5eb9812cf903348d83ad9fa35
dominick.paez <superrrstyxx@gmail.com>
2018-10-04 08:57:33 -0700

    Update

--- CommandLine/Robot1.java
+++ CommandLine/Robot1.java
@@ -1,2 +1,18 @@
-public class robot1 {
+import edu.cmu.ri.createlab.hummingbird.HummingbirdRobot;
+
+public class Robot1 extends HummingbirdRobot {
+    public static void main(String[] args) throws InterruptedException{
+        HummingbirdRobot bob = new HummingbirdRobot();
+
+
+        bob.setMotorVelocity(1,0);
+        int x = 1;
+        while(x<100000) {
+            int s = bob.getSensorValue(1);
+            System.out.println("Sensor: " + s);
+        }
+
+
+        bob.disconnect();
+    }
 }
