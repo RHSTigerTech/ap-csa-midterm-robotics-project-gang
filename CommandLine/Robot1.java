ac750bb3d4c34ae2121a8c71b932ba1cf8cde706
ivan krat <ivan.krat@stydent.rjuhsd.us>
2018-10-04 08:53:39 -0700

    done stuff 3

--- CommandLine/Robot1.java
+++
@@ -1,7 +0,0 @@
-import edu.cmu.ri.createlab.hummingbird.HummingbirdRobot;
-
-public class Robot1 extends HummingbirdRobot {
-    public static void main(String[] args) throws InterruptedException{
-        HummingbirdRobot cheesyBob = new HummingbirdRobot();
-    }
-}
