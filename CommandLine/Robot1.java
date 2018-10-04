a342962e3c1e3eabd48b41967d1e1bf570e06834
dominick.paez <superrrstyxx@gmail.com>
2018-10-04 08:46:16 -0700

    Update

--- CommandLine/Robot1.java
+++ CommandLine/Robot1.java
@@ -1,7 +1,2 @@
-import edu.cmu.ri.createlab.hummingbird.HummingbirdRobot;
-
-public class Robot1 extends HummingbirdRobot {
-    public static void main(String[] args) throws InterruptedException{
-        HummingbirdRobot cheesyBob = new HummingbirdRobot();
-    }
+public class robot1 {
 }
